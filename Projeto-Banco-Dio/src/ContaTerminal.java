e80350e542e70cc0ae1d4242a8569bc7ae135c6e
WMBarbosa <wesleymario01@gmail.com>
2024-04-26 15:34:06 -0300

    Commit Desafio Java Banco

---
+++ Projeto-Banco-Dio/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        var nomeCliente = leitura.next();
+        System.out.println("Digite o numero da conta: ");
+        var numeroConta = leitura.nextInt();
+        System.out.println("Digite o numero de sua Agência ");
+        var agencia = leitura.next();
+        System.out.println("Digite seu Saldo: ");
+        var saldo = leitura.nextDouble();
+
+        ContaBanco conta = new ContaBanco(numeroConta, agencia, nomeCliente, saldo);
+
+        System.out.println("Olá ".concat(conta.getNomeCliente()).concat(",")
+                + " obrigado por criar uma conta em nosso banco, sua agência é".concat(" ").concat(conta.getAgencia())
+                + ", conta".concat(" ") + conta.getNumero() + " e seu saldo".concat(" ") + conta.getSaldo()
+                + " já está disponivel para saque");
+    }
+}
