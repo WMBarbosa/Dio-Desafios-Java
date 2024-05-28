908d0c87124b9d5b93c822966b24c9aa9b0ceef9
WMBarbosa <wesleymario01@gmail.com>
2024-05-28 11:02:51 -0300

    Commit Desafio

--- Projeto-Banco-Digital/src/Principal/Main.java
+++ Projeto-Banco-Digital/src/Principal/Main.java
@@ -2,23 +2,121 @@ package Principal;
 
 import Service.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Cliente venilton = new Cliente();
-        venilton.setNome("Wesley");
+        Scanner sc = new Scanner(System.in);
 
-        Conta cc = new ContaCorrente(venilton);
-        Conta poupanca = new ContaPoupanca(venilton);
+        int opcao;
+        Cliente cliente = null;
+        Conta conta = null;
 
-        cc.depositar(100);
-        cc.transferir(100, poupanca);
+        do {
+            System.out.println("==== Serviços ====");
+            System.out.println("1- Criar uma conta");
+            System.out.println("2- Depositar");
+            System.out.println("3- Sacar");
+            System.out.println("4- Transferir");
+            System.out.println("5- Verificar Extrato");
+            System.out.println("0- Sair");
 
-        cc.imprimirExtrato();
-        poupanca.imprimirExtrato();
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Qual conta deseja criar? ");
+                    System.out.println("1- Conta Corrente");
+                    System.out.println("2- Conta Poupanca");
+                    int escolha = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Digite o nome do Cliente: ");
+                    String nomeCliente = sc.nextLine();
+
+                    cliente = new Cliente();
+                    cliente.setNome(nomeCliente);
+
+                    if (escolha == 1) {
+                        conta = new ContaCorrente(cliente);
+                    } else if (escolha == 2) {
+                        conta = new ContaPoupanca(cliente);
+                    }
+
+                    System.out.println("Conta criada com sucesso! Número: " + conta.getNumero());
+                    break;
+
+                case 2:
+                    if (conta == null) {
+                        System.out.println("Nenhuma conta criada.");
+                    } else {
+                        System.out.println("Digite o valor do depósito: ");
+                        double deposito = sc.nextDouble();
+
+                        conta.depositar(deposito);
+                        System.out.println("Saldo atual: " + conta.getSaldo());
+                    }
+                    break;
+
+                case 3:
+                    if (conta == null) {
+                        System.out.println("Nenhuma conta criada.");
+                    } else {
+                        System.out.println("Digite o valor para saque: ");
+                        double saque = sc.nextDouble();
+
+                        conta.sacar(saque);
+                        System.out.println("Saldo atual: " + conta.getSaldo());
+                    }
+                    break;
+
+                case 4:
+                    if (conta == null) {
+                        System.out.println("Nenhuma conta criada.");
+                    } else {
+                        System.out.println("Digite o valor para transferir: ");
+                        double valorTransferencia = sc.nextDouble();
+                        sc.nextLine();
+
+                        System.out.println("Digite o número da conta de destino:");
+                        int numeroContaDestino = sc.nextInt();
+
+                        Conta contaDestino = Conta.encontrarConta(numeroContaDestino);
+
+                        if (contaDestino != null) {
+                            conta.transferir(valorTransferencia, contaDestino);
+                            System.out.println("Transferência realizada com sucesso!");
+                        } else {
+                            System.out.println("Conta de destino não encontrada.");
+                        }
+                    }
+                    break;
+
+                case 5:
+                    if (conta == null) {
+                        System.out.println("Nenhuma conta criada.");
+                    } else {
+                        System.out.println("Extrato da conta:");
+                        conta.imprimirExtrato();
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } while (opcao != 0);
+
+        sc.close();
+    }
+
+
     }
 }
 
--- Projeto-Banco-Digital/src/Service/Banco.java
+++ Projeto-Banco-Digital/src/Service/Banco.java
@@ -2,11 +2,33 @@ package Service;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Data
 public class Banco {
     private String nome;
     private List<Conta> contas;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionaConta(Conta conta) {
+            this.contas.add(conta);
+    }
+
+    public void removeConta(Conta conta) {
+        if (contas.isEmpty()){
+            System.out.println("A lista está vazia");
+        } else {
+            this.contas.remove(conta);
+        }
+    }
+
+    public void tamanhoContas() {
+        System.out.println(contas.size());
+    }
 
 }
